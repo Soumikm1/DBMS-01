45347d6df798458dea7fc519d11026127ca37321
Soumik Mandal <120877003+Soumikm1@users.noreply.github.com>
2024-02-27 19:02:38 +0000

    urls update 1

--- fest/fest/urls.py
+++ fest/fest/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from festapp import views
+
+admin.site.site_header = "FEsT Admin"
+admin.site.site_title = "FEsT Admin Portal"
+admin.site.index_title = "Welcome to FEsT Portal"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('festapp.urls')),
 ]
