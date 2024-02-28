84935583e8e7a85d29e13d9b727735950482e4fc
Soumik Mandal <120877003+Soumikm1@users.noreply.github.com>
2024-02-28 06:20:42 +0000

    Update 02

--- fest/festapp/urls.py
+++ fest/festapp/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from fest import views
 
+admin.site.site_header = "FEsT Admin"
+admin.site.site_title = "FEsT Admin Portal"
+admin.site.index_title = "Welcome to FEsT Portal"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
+    path('', include('festapp.urls')),
 ]
--- fest/festapp/views.py
+++ fest/festapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from festapp.models import Item_List
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request, 'index.html')
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user_pages = {
+            'owner': '/',
+            'daily_manager': '/reportSale',
+            'order_manager': '/viewInventory',
+            'sales_manager': '/reportSale',
+            'revenue_manager': '/viewGraph',
+        }
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = request.GET.get('next', '/')
+            redirect_url = user_pages.get(username, '/')
+            return redirect(redirect_url)
+        else:
+            return render(request, 'login.html')
+    return render(request, 'login.html')
